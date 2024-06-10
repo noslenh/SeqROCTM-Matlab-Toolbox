e63a3aceb1fe032e8302e0cc7b30995dbc4f6ae9
Noslen Hernandez <noslenh@gmail.com>
2024-06-10 17:48:23 +0200

    small change to the init file

--- init.m
+++ init.m
@@ -1,5 +1,7 @@
+% Execute this file to add SeqROCTM to the Matlab path and star working
+
 % get the current directory
 current_directory = pwd;
 
-% add the folder SeqROCT with its subfolder to the path
+% add the folder SeqROCTM with its subfolder to the path
 addpath(genpath(current_directory));
